95dc1f831e61a6f61bc8e7b7b059b5b58f01db4d
tracy-nguyen319 <111233185+tracy-nguyen319@users.noreply.github.com>
2024-02-07 23:14:00 +0700

    Add files via upload

---
+++ Day1-WWCode-2024challenge.py
@@ -0,0 +1,16 @@
+#Gather input
+a = input('Enter the first value: ')
+b = input('Enter the second value: ')
+
+#Before swap
+print('Before swapping: ' + a + ', ' + b)
+
+#Find the middleman
+c = a
+
+#Swap
+a = b
+b = c
+
+#After swap
+print('After swapping: '+ a +', ' + b)
